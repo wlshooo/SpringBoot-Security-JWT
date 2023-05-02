d6f9e003cee967267dcec4274c8792c2cce7be98
wlshooo <kswlsgh@naver.com>
2023-05-02 14:36:09 +0900

    sqlexception

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,4 +11,31 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨";
+    }
+
 }
